81c13de028e4e8895f2d46fdcee052402e597a2f
jcf94 <xff252595680@gmail.com>
2018-09-12 18:15:10 +0800

    Update ThreadPool to std::future style

--- ThreadPool/ThreadPool.cpp
+++ ThreadPool/ThreadPool.cpp
@@ -4,21 +4,39 @@ LANG   : G++
 PROG   : THREADPOOL
 ************************************************ */
 
-#include "ThreadRunner.h"
+#include "ThreadPool.h"
 
 ThreadPool::ThreadPool(int num_threads)
     : stop_(false)
 {
     for (int i=0;i<num_threads;i++)
-        runners_.push_back(std::thread(ThreadRunner(*this)));
+        runners_.emplace_back([this]()
+        {
+            for (;;)
+            {
+                std::function<void()> task;
+                {
+                    std::unique_lock<std::mutex> lock(this->tasks_mutex_);
+                    this->cv_.wait(lock,
+                        [this]{return this->stop_ || !this->tasks_.empty();});
+                    
+                    if (this->stop_ && this->tasks_.empty()) return;
+                    task = std::move(this->tasks_.front());
+                    this->tasks_.pop();
+                }
+                task();
+            }
+        });
 }
 
 ThreadPool::~ThreadPool()
 {
-    stop_ = true;
+    {
+        std::unique_lock<std::mutex> lock(tasks_mutex_);
+        stop_ = true;
+    }
     cv_.notify_all();
-    for (auto &i:runners_)
-        i.join();
+    for (auto &i:runners_) i.join();
 }
 
 void ThreadPool::wait()
--- ThreadPool/ThreadPool.h
+++ ThreadPool/ThreadPool.h
@@ -12,6 +12,9 @@ PROG	: THREADPOOL_H
 #include <condition_variable>
 #include <vector>
 #include <queue>
+#include <future>
+
+#define RETURN_TYPE typename std::result_of<Function(Args...)>::type
 
 class ThreadPool
 {
@@ -19,15 +22,25 @@ public:
     ThreadPool(int num_threads);
     ~ThreadPool();
 
-    template<class F>
-    void add_task(F f)
+    template<class Function, class... Args>
+    std::future<RETURN_TYPE> add_task(Function&& f, Args&&... args)
     {
+        auto task = std::make_shared<std::packaged_task<RETURN_TYPE()>>(
+            std::bind(std::forward<Function>(f), std::forward<Args>(args)...));
+        
+        std::future<RETURN_TYPE> res = task->get_future();
+
         {
             std::unique_lock<std::mutex> lock(tasks_mutex_);
 
-            tasks_.push(std::function<void()>(f));
+            if (stop_) throw std::runtime_error("ThreadPool is stopped.");
+
+            tasks_.emplace([task](){(*task)();});
         }
+
         cv_.notify_one();
+
+        return res;
     }
     void wait();
 
